f04007a02bec10d19d2d411691af7b643e3e360f
PanktiPatel99 <panktip.mobio@gmail.com>
2022-09-16 12:34:18 +0530

    create auto suggest functionality from dynamic data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 
-import Images from "./Components/Images";
+// import Images from "./Components/Images";
+import ImageResult from "./Components/ImageResult";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">Image Gallery</header>
-      <Images />
+      {/* <Images /> */}
+      <ImageResult />
     </div>
   );
 };
